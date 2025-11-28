6208f89aed0db40be57d9c29c5704a1ae5d0fad0
luispabloln <65974204+luispabloln@users.noreply.github.com>
2025-11-28 00:16:36 -0400

    Update dashboard_ventas.py

--- dashboard_ventas.py
+++ dashboard_ventas.py
@@ -6,7 +6,7 @@ import datetime
 import os
 
 # --- CONFIGURACIÓN ---
-st.set_page_config(page_title="Hupa Dashboard", page_icon="💎", layout="wide")
+st.set_page_config(page_title="Hupa Dashboard", page_icon="🚛", layout="wide")
 
 # --- ESTILOS CSS ---
 st.markdown("""
